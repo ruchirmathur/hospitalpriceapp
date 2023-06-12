62a4b354aadbed016e34fd8ee11f4d03bf638f8e
ruchirmathur <ruchirmat@gmail.com>
2023-06-12 10:42:52 -0700

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -16,7 +16,7 @@ export class LoginComponent implements OnInit {
       {
         environment_id: '1ca92fb8-86bf-492d-b55f-c45545517059',
         client_id: '6772740f-31e8-4717-a89a-ad974a29ace7',
-        redirect_uri: 'http://localhost:4200/dashboard',
+        redirect_uri: 'https://gray-tree-0f6a0900f.3.azurestaticapps.net/dashboard',
         post_logout_redirect_uri: 'http://localhost:4200',
         api_uri: 'https://api.pingone.com',
         auth_uri: 'https://auth.pingone.com'
